59dacae3545f7b963639d791af0330d46ef2dc8d
Library of Fantasia <library-of-fantasia@proton.me>
2026-09-27 01:40:53 +0800

    deploy: c3b07a222b5c670e2fb0c1ed9076a56befc72833

--- wasm/__wasm_split.______________________.js
+++ wasm/__wasm_split.______________________.js
@@ -4,8 +4,8 @@ let sharedImports = undefined;
 function getSharedImports() {
     if (sharedImports === undefined) {
         sharedImports = { __wasm_split: {  } };
-        const { memory, __wasm_split_shared0, __indirect_function_table,  } = initSync(undefined, undefined);
-        Object.assign(sharedImports.__wasm_split, { memory, __wasm_split_shared0, __indirect_function_table,  });
+        const { __wasm_split_shared0, __indirect_function_table, memory,  } = initSync(undefined, undefined);
+        Object.assign(sharedImports.__wasm_split, { __wasm_split_shared0, __indirect_function_table, memory,  });
     }
     return sharedImports;
 }
